67352844ae36d95b5900cfc3eaddd594e350e432
Samir-Romdhani <romdhanisamir94@gmail.com>
2017-07-18 12:23:28 +0100

    commentaire tna7ou

--- web.js
+++ web.js
@@ -3,16 +3,16 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
+/*
 app.get('/', function(request, response) {
 response.send('Hello World!!!! AAAAAAAAAAAA') ;
 }) ;
-
-/*
+*/
 var connection = mysql.createConnection({
-    host: 'jlg7sfncbhyvga14.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'e9rhv9wpwaeuqxe9',
-    password: 'nkztklojrw7d2fr3',
-    database: 'fnyv85ghptt684va'
+    host: 'thzz882efnak0xod.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'vbebknxc303ntx0f',
+    password: 'nnzetk5uihgms2l8',
+    database: 'vti0cwdbw6zs5nka'
 });
 
 connection.connect();
@@ -28,7 +28,7 @@ app.get('/', function(request, response) {
         
         
 });
-*/
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
